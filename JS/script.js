e7372e7daf4a827c756fcd7147cf9bf17cd411b6
Ganiyu Olowookere <wedesign4u11@gmail.com>
2023-09-27 18:13:38 +0100

    .

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,7 @@ function myFunction() {
   document.getElementById("MyTopnav").classList.toggle("responsive");
   document.getElementById("OpenNclose").classList.toggle("iconClose");
 }
+
+function congratsactive() {
+  document.getElementById("overlay").classList.toggle("congratsOverlay");
+}
